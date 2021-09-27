baeaa9e41795c91428ce21c461ce17815dab508d
Haruto Konno <qingdoujinye@gmail.com>
2021-09-27 16:02:07 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:campe_app/view/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'カンペアップ',
       home: HomePage(),
     );
