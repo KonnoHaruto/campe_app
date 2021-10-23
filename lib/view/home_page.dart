1c334a3b275eb3f4424e5f7a2349487870e0ebdc
Haruto Konno <qingdoujinye@gmail.com>
2021-10-23 09:59:03 +0900

    firestoreを追加

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,14 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
-  const HomePage({Key? key}) : super(key: key);
+import '../reference.dart';
+import 'loading_page.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  TextEditingController textController = TextEditingController();
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('campe'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Flexible(
+                child: StreamBuilder(
+                    // streamにデータが流れてくると自動で再描画される
+                    stream: campe.snapshots(),
+                    // QuerySnapshotはCollectionReferenceで
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return const LoadingPage();
+                      }
+                      return ListView(
+                        // data!はnullが入らないことを明示するためのもの・
+                        // 上のif文でnullが入った際の処理をしている。
+                        children: snapshot.data!.docs.map((campes) {
+                          return Center(
+                            child: ListTile(
+                              title: Text(campes['content']),
+                              onLongPress: () {
+                                campes.reference.delete();
+                                //ignore: avoid_print
+                                print('deleted: $campes');
+                              },
+                            ),
+                          );
+                        }).toList(),
+                      );
+                    }))
+          ],
+        ),
+      ),
     );
   }
 }
