07d4dd0aaa820d67f4466b5df2465f2aede370e5
Haruto Konno <qingdoujinye@gmail.com>
2021-11-17 15:45:43 +0900

    PageViewを追加

--- lib/view/setting_page.dart
+++ lib/view/setting_page.dart
@@ -1,18 +1,25 @@
+import 'package:campe_app/reference.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SettingPage extends ConsumerWidget {
+class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('設定'),
-        elevation: 0,
-      ),
-      body: Container(
-        color: Colors.white,
-      ),
-    );
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+
+    return PageView(
+        scrollDirection: Axis.vertical, 
+        controller: controller, 
+        children: const [
+          Icon(Icons.close)
+        ]);
   }
 }
