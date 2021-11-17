ef23f6e7717f2a0ac850a3bd441dc14057fed726
Haruto Konno <qingdoujinye@gmail.com>
2021-11-17 09:37:44 +0900

    modal_progress_spinerを削除

--- lib/view/adding_page.dart
+++ lib/view/adding_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:campe_app/reference.dart';
-import 'package:modal_progress_hud_alt/modal_progress_hud_alt.dart';
 
 class AddingPage extends StatefulWidget {
   const AddingPage({Key? key}) : super(key: key);
@@ -13,19 +12,6 @@ class AddingPage extends StatefulWidget {
 class _AddingPageState extends State<AddingPage> {
   final textController = TextEditingController();
 
-  bool showSpiner = false;
-
-  void startSpiner() {
-    setState(() {
-      showSpiner = true;
-    });
-  }
-
-  void stopSpiner() {
-    setState(() {
-      showSpiner = false;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,64 +19,58 @@ class _AddingPageState extends State<AddingPage> {
       appBar: AppBar(
         title: const Text('カンペ作成'),
       ),
-      body: ModalProgressHUD(
-        inAsyncCall: showSpiner,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                height: 600,
-                child: TextField(
-                  controller: textController,
-                  // ignore: unnecessary_const
-                  decoration: const InputDecoration(
-                    hintText: "入力してください",
-                  ),
-                  autofocus: true, //ページ遷移時に自動フォーカス
-                  maxLines: null,
-                  textAlign: TextAlign.left,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              height: 600,
+              child: TextField(
+                controller: textController,
+                // ignore: unnecessary_const
+                decoration: const InputDecoration(
+                  hintText: "入力してください",
                 ),
+                autofocus: true, //ページ遷移時に自動フォーカス
+                maxLines: null,
+                textAlign: TextAlign.left,
               ),
-              SafeArea(
-                child: ElevatedButton(
-                  onPressed: () {
-                    startSpiner();
-                    if (textController.text == "") {
-                      showDialog(
-                          context: context,
-                          builder: (_) {
-                            return CupertinoAlertDialog(
-                              title: const Text('テキストが未入力です'),
-                              content: const Text('テキストの入力を完了させてください'),
-                              actions: [
-                                CupertinoDialogAction(
-                                  child: const Text('Ok'),
-                                  isDestructiveAction: false,
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                    stopSpiner();
-                                  },
-                                ),
-                              ],
-                            );
-                          });
-                    } else {
-                      campeRef.add({'content': textController.text, 'createdAt': DateTime.now()});
-                      textController.clear();
-                      Navigator.pop(context);
-                      stopSpiner();
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
-                  ),
-                  child: const Text('カンペを追加'),
+            ),
+            SafeArea(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (textController.text == "") {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return CupertinoAlertDialog(
+                            title: const Text('テキストが未入力です'),
+                            content: const Text('テキストの入力を完了させてください'),
+                            actions: [
+                              CupertinoDialogAction(
+                                child: const Text('Ok'),
+                                isDestructiveAction: false,
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  } else {
+                    campeRef.add({'content': textController.text, 'createdAt': DateTime.now()});
+                    textController.clear();
+                    Navigator.pop(context);
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
                 ),
+                child: const Text('カンペを追加'),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
