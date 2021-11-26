b164c04e1afa80ef73de7ead11bb1608ecc2a57c
Haruto Konno <qingdoujinye@gmail.com>
2021-11-26 16:39:14 +0900

    feat(home_page.dart): デザインやレイアウトを変更
    BREAKING CHANGE: デザインやレイアウトを変更

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -19,14 +19,18 @@ class _HomePageState extends State<HomePage> {
   final _auth = FirebaseAuth.instance;
   User? user = FirebaseAuth.instance.currentUser;
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawerEdgeDragWidth: 0.0,
       appBar: AppBar(
         elevation: 0,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25) ,
+          ),
+        ),
         title: const Text(
           'カンペ 一覧',
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -49,66 +53,72 @@ class _HomePageState extends State<HomePage> {
               Navigator.pop(context);
             }),
       ),
-      drawer: Container(width: 100),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Flexible(
-                  child: StreamBuilder(
-                      stream: campeRef
-                          .orderBy(
-                            'createdAt',
-                            descending: false,
-                          )
-                          .snapshots(),
-                      builder:
-                          (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                        if (!snapshot.hasData) {
-                          return const CircularProgressIndicator();
-                        }
-                        return ListView(
-                          children: snapshot.data!.docs.map((campes) {
-                            if (campes['content'] == null) {
-                              campes.reference.update({
-                                'content': '( 未入力 )',
-                              });
-                            } else if (campes['content'] == bool) {
-                              campes.reference.update({
-                                'content': '( 未入力 )',
-                              });
-                            }
-                            return Center(
-                              child: Card(
-                                elevation: 8,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
+        child: Container(
+          padding: const EdgeInsets.only(top: 15),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                const FaIcon(FontAwesomeIcons.peopleCarry),
+                Flexible(
+                  child: Container(
+                    padding: const EdgeInsetsDirectional.only(top: 15),
+                    child: StreamBuilder(
+                        stream: campeRef
+                            .orderBy(
+                              'createdAt',
+                              descending: false,
+                            )
+                            .snapshots(),
+                        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (!snapshot.hasData) {
+                            return const CircularProgressIndicator();
+                          }
+                          return ListView( 
+                            children: snapshot.data!.docs.map((campes) {
+                              if (campes['content'] == null) {
+                                campes.reference.update({
+                                  'content': '( 未入力 )',
+                                });
+                              } else if (campes['content'] == bool) {
+                                campes.reference.update({
+                                  'content': '( 未入力 )',
+                                });
+                              }
+                              return Center(
+                                child: Card(
+                                  elevation: 1,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: ListTile(
+                                    dense: false,
+                                    title: Text(campes['content'].toString()),
+                                    onLongPress: () {
+                                      campes.reference.delete();
+                                    },
+                                    onTap: () async {
+                                      var updatedContent =
+                                          await Navigator.push(context,
+                                              MaterialPageRoute(builder: (context) {
+                                        return UpdatePage(
+                                            oldText: campes['content']);
+                                      }));
+                                      campes.reference.update({
+                                        'content': updatedContent,
+                                        'updatedAt': DateTime.now()
+                                      });
+                                    },
+                                  ),
                                 ),
-                                child: ListTile(
-                                  dense: false,
-                                  title: Text(campes['content'].toString()),
-                                  onLongPress: () {
-                                    campes.reference.delete();
-                                  },
-                                  onTap: () async {
-                                    var updatedContent = await Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) {
-                                      return UpdatePage(
-                                          oldText: campes['content']);
-                                    }));
-                                    campes.reference.update({
-                                      'content': updatedContent,
-                                      'updatedAt': DateTime.now()
-                                    });
-                                  },
-                                ),
-                              ),
-                            );
-                          }).toList(),
-                        );
-                      })),
-            ],
+                              );
+                            }).toList(),
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
