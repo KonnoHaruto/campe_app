fd1fb2be7a375d281f04f7a806f179777385b885
KonnoHaruto <letsmossmoss@gmail.com>
2022-02-07 19:54:01 +0900

    #76 PreviewScreenをリファクタリング

--- lib/view/screens/preview_screen.dart
+++ lib/view/screens/preview_screen.dart
@@ -1,56 +1,46 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:campe_app/controller/campe_list_controller/campe_lsit_controller_provider.dart';
+import 'package:campe_app/exception/custom_exception.dart';
+import 'package:campe_app/view/campe_list_error.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../reference.dart';
-
-class PreviewScreen extends StatefulWidget {
+class PreviewScreen extends ConsumerWidget {
   const PreviewScreen({Key? key}) : super(key: key);
 
   @override
-  State<PreviewScreen> createState() => _PreviewScreenState();
-}
-
-class _PreviewScreenState extends State<PreviewScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 0);
-
+  Widget build(BuildContext context, WidgetRef ref) {
+    final campeState = ref.watch(campeListControllerProvider);
     return Scaffold(
-      body: Center(
-        child: StreamBuilder<QuerySnapshot>(
-            stream:
-                campeRef.orderBy('createdAt', descending: false).snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return const Text('Loading...');
-                default:
-                  return PageView.builder(
-                      controller: controller,
-                      scrollDirection: Axis.vertical,
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, campe) {
-                        final campes = snapshot.data!.docs[campe];
-                        return ListTile(
-                          title: Center(
-                            child: Text(
-                              campes['content'],
-                              style: const TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        );
-                      });
-              }
-            }),
+      body: campeState.when(
+        data: (campes) => campes.isEmpty
+            ? const Center(
+                child: Text(
+                  '＋ボタンを押してカンペを作成',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              )
+            : PageView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: campeState.value?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final campe = campes[index];
+                  return ListTile(
+                    title: Center(
+                      child: Text(campe.name, 
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ),
+                  );
+                }),
+        error: (error, _) => CampeListError(
+          message: error is CustomException
+          ? error.message!
+          : 'Something went wrong!',
+        ),
+        loading: () => const Center(child: CircularProgressIndicator())
       ),
     );
   }
 }
-
