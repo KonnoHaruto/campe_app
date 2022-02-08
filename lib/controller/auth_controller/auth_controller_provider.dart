3284e2f68a54cf34081e2cb24afd0103a96dc417
KonnoHaruto <letsmossmoss@gmail.com>
2022-02-08 20:19:25 +0900

    #83 auth_controllerを修正

--- lib/controller/auth_controller/auth_controller_provider.dart
+++ lib/controller/auth_controller/auth_controller_provider.dart
@@ -2,6 +2,5 @@ import 'package:campe_app/controller/auth_controller/auth_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// ..はカスケード記法
 final authControllerProvider = StateNotifierProvider<AuthController, User?>(
-  (ref) => AuthController(ref.read)..appStarted());
+  (ref) => AuthController(ref.read));
