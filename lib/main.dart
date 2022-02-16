30c0c23c0f45dfd5485e7e8c12338a80030c6755
松坂波季 <matuharu@matsuzakahakinoMacBook-Pro.local>
2022-02-16 22:45:44 +0900

    バージョンの取得とアプリアイコンの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:campe_app/my_app.dart';
+import 'package:campe_app/repositories/package_info_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
-  await Firebase.initializeApp();
+  late final PackageInfo packageInfo;
+  await Future.wait([
+    Future  (()async => packageInfo =  await PackageInfo.fromPlatform()),
+    Firebase.initializeApp(),
+  ]);
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      overrides: [
+        packageInfoRepositoryProvider
+            .overrideWithValue(PackageInfoRepository(packageInfo)),
+      ],
+      child: const MyApp(),
     ),
   );
 }
