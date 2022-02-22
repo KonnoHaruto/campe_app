e3321784635db612a485800c4c889d467964dbd1
KonnoHaruto <letsmossmoss@gmail.com>
2022-02-22 15:03:04 +0900

    #96 設定画面にアプリバージョンを表示するように変更

--- lib/view/screens/setting_screen.dart
+++ lib/view/screens/setting_screen.dart
@@ -1,11 +1,15 @@
+import 'package:campe_app/repositories/package_info_repository.dart';
+import 'package:campe_app/view/dialog/warning_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends ConsumerWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appVersion = ref.watch(packageInfoRepositoryProvider).appVersion;
     return Scaffold(
       appBar: AppBar(
         title: const Text('設定画面'),
@@ -15,7 +19,23 @@ class SettingScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.indigo,
       ),
-      body: null,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: const FaIcon(FontAwesomeIcons.info),
+              title: const Text('アプリバージョン'),
+              subtitle: Text(appVersion),
+            ),
+            ListTile(
+              leading: const FaIcon(FontAwesomeIcons.signOutAlt),
+              title: const Text('アプリを退会'),
+              onTap: () => WarningDialog.show(context),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
